910c3826ca715c73819736e790bc6db3a512afc1
edgarjoserojaslopez <ejrl64@yahoo.com>
2021-04-03 19:21:57 -0400

    Commit 03042021

--- src/components/pages/NosotrosPage/JuntaPage.js
+++ src/components/pages/NosotrosPage/JuntaPage.js
@@ -95,6 +95,19 @@ export default function HistoriaPage() {
                 </TableContainer>
               </Grid>
               <Grid item style={{ padding: "16px", marginBottom: "24px" }}>
+                <p style={{ textAlign: "justify" }}>
+                  <Typography variant="body1">
+                    Consejo de Vigilancia:
+                  </Typography>{" "}
+                  Es el órgano encargado de velar por el estricto cumplimiento
+                  de la Ley de Cajas de Ahorro y Fondos de Ahorro, su
+                  Reglamento, Los Estatutos, Reglamentos Internos y las
+                  decisiones de la Asamblea General de Delegados, así como del
+                  correcto funcionamiento, administración y buen manejo de los
+                  fondos de la Asociación.
+                </p>
+              </Grid>
+              <Grid item style={{ padding: "16px", marginBottom: "24px" }}>
                 <TableContainer component={Paper}>
                   <Table aria-label="simple table">
                     <TableHead>
@@ -128,19 +141,7 @@ export default function HistoriaPage() {
                 </TableContainer>
               </Grid>
 
-              <Grid item style={{ padding: "16px", marginBottom: "24px" }}>
-                <p style={{ textAlign: "justify" }}>
-                  <Typography variant="body1">
-                    Consejo de Vigilancia:
-                  </Typography>{" "}
-                  Es el órgano encargado de velar por el estricto cumplimiento
-                  de la Ley de Cajas de Ahorro y Fondos de Ahorro, su
-                  Reglamento, Los Estatutos, Reglamentos Internos y las
-                  decisiones de la Asamblea General de Delegados, así como del
-                  correcto funcionamiento, administración y buen manejo de los
-                  fondos de la Asociación.
-                </p>
-              </Grid>
+              
             </Grid>
           </Box>
         </Grid>
